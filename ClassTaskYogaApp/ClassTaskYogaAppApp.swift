f9ad548cb8e8e0cd9dfb4f7c0f7ac0da957e8af1
Naif Ghannam <dhdfaui@gmail.com>
2025-03-16 14:54:13 +0300

    Initial Commit

---
+++ ClassTaskYogaApp/ClassTaskYogaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassTaskYogaAppApp.swift
+//  ClassTaskYogaApp
+//
+//  Created by Mac on 16/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ClassTaskYogaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
